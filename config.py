7e6fbbcb139e8fa611e4970b6c661a8be8771542
pmyag15 <pmyag15@nottingham.ac.uk>
2026-02-24 18:44:44 +0000

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 DATA_SOURCE = "local"  # or "yahoo"
 CURRENCY_PAIRS = ["EURUSD=X", "GBPUSD=X", "AUDUSD=X", "USDCAD=X", "USDJPY=X"]
 LOCAL_FILE_MAPPING = {
-    "EURUSD=X": "data/EURUSD.csv",
-    "GBPUSD=X": "data/GBPUSD.csv",
-    "AUDUSD=X": "data/AUDUSD.csv",
-    "USDCAD=X": "data/USDCAD.csv",
-    "USDJPY=X": "data/USDJPY.csv"
+    "EURUSD=X": "data/EURUSD=X.csv",
+    "GBPUSD=X": "data/GBPUSD=X.csv",
+    "AUDUSD=X": "data/AUDUSD=X.csv",
+    "USDCAD=X": "data/USDCAD=X.csv",
+    "USDJPY=X": "data/USDJPY=X.csv"
 }
